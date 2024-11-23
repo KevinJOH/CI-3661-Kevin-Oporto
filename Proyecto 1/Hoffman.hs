80cbcdd8be93cca43431fd177a546b9425debd08
KevinJOH <13-11007@usb.ve>
2024-11-23 02:25:21 -0400

    Agregar instancia Read

--- Proyecto 1/Hoffman.hs
+++ Proyecto 1/Hoffman.hs
@@ -1,11 +1,45 @@
 {-# LANGUAGE InstanceSigs #-}
 import qualified Data.Map as Map
+import Text.Read (Read(..), ReadPrec, Lexeme(Ident, Punc), lexP, parens, prec, readPrec, step)
+import Control.Applicative ((<|>))
+
 data Hoffman = Hoja Char | Nodo Hoffman Hoffman
   deriving Eq
 
 instance Show Hoffman where
   show (Hoja a) = "Hoja " ++ show a
   show (Nodo izq der) = "Nodo (" ++ show izq ++ ") (" ++ show der ++ ")"
+
+instance Read Hoffman where
+  readsPrec :: Int -> String -> [(Hoffman, String)]
+  readsPrec _ input = readHoffman input
+
+  readPrec :: Text.Read.ReadPrec Hoffman
+  readPrec = parens $ (prec 10 $ do
+                        Ident "Hoja" <- lexP
+                        c <- step readPrec
+                        return (Hoja c))
+                      <|> (prec 10 $ do
+                        Ident "Nodo" <- lexP
+                        Punc "(" <- lexP
+                        izq <- readPrec
+                        Punc ")" <- lexP
+                        Punc "(" <- lexP
+                        der <- readPrec
+                        Punc ")" <- lexP
+                        return (Nodo izq der))
+
+readHoffman :: String -> [(Hoffman, String)]
+readHoffman input = case lex input of
+  [("Hoja", resto)] -> [(Hoja char, resto'') | (char, resto'') <- reads resto]
+  [("Nodo", resto)] -> 
+    [(Nodo izq der, resto'''')
+    | ("(", resto') <- lex resto
+    , (izq, resto'') <- readHoffman resto'
+    , (",", resto''') <- lex resto''
+    , (der, resto'''') <- readHoffman resto'''
+    , (")", resto'''') <- lex resto''']
+  _ -> []
 
 nuevoHoffman :: Char -> Hoffman
 nuevoHoffman a = Hoja a
