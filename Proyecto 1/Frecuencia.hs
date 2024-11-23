9c3b2671ac755b89f8877bddf0773b1f9176f39f
KevinJOH <13-11007@usb.ve>
2024-11-23 02:36:39 -0400

    Agregar documentacion

--- Proyecto 1/Frecuencia.hs
+++ Proyecto 1/Frecuencia.hs
@@ -1,22 +1,30 @@
 {-# LANGUAGE InstanceSigs #-}
+
+-- Definición del tipo de datos Frecuencia que asocia un valor con su frecuencia.
 data Frecuencia a = Frecuencia a Int
   deriving Eq
 
+-- Instancia de la clase Show para Frecuencia, permite convertir una Frecuencia a una cadena de caracteres.
 instance Show a => Show (Frecuencia a) where
   show :: Frecuencia a -> String
   show (Frecuencia x y) = "Valor: " ++ show x ++ " - Frecuencia: " ++ show y
 
+-- Instancia de la clase Ord para Frecuencia, permite comparar frecuencias basándose en el número de ocurrencias.
 instance Eq a => Ord (Frecuencia a) where
   compare (Frecuencia _ x) (Frecuencia _ y) = compare x y
 
+-- Función para iniciar una frecuencia para un valor dado, con una ocurrencia.
 iniciarFrecuencia :: Eq a => a -> Frecuencia a
 iniciarFrecuencia x = Frecuencia x 1
 
+-- Función para contar el número de ocurrencias de un valor en una lista y devolver una Frecuencia.
 contar :: Eq a => a -> [a] -> Frecuencia a
 contar x xs = Frecuencia x (length (filter (== x) xs))
 
+-- Función para obtener el valor asociado a una Frecuencia.
 valor :: Frecuencia a -> a
 valor (Frecuencia x _) = x
 
+-- Función para obtener la frecuencia (el número de ocurrencias) de un valor en una Frecuencia.
 frecuencia :: Frecuencia a -> Int
 frecuencia (Frecuencia _ x) = x
--- Proyecto 1/Hoffman.hs
+++ Proyecto 1/Hoffman.hs
@@ -3,13 +3,16 @@ import qualified Data.Map as Map
 import Text.Read (Read(..), ReadPrec, Lexeme(Ident, Punc), lexP, parens, prec, readPrec, step)
 import Control.Applicative ((<|>))
 
+-- Definición del tipo de datos para el árbol de Hoffman.
 data Hoffman = Hoja Char | Nodo Hoffman Hoffman
   deriving Eq
 
+-- Instancia de la clase Show para el tipo Hoffman, permitiendo la conversión del árbol de Hoffman a una representación en cadena.
 instance Show Hoffman where
   show (Hoja a) = "Hoja " ++ show a
   show (Nodo izq der) = "Nodo (" ++ show izq ++ ") (" ++ show der ++ ")"
 
+-- Instancia de la clase Read para el tipo Hoffman, permitiendo la conversión de una cadena a un árbol de Hoffman.
 instance Read Hoffman where
   readsPrec :: Int -> String -> [(Hoffman, String)]
   readsPrec _ input = readHoffman input
@@ -29,6 +32,7 @@ instance Read Hoffman where
                         Punc ")" <- lexP
                         return (Nodo izq der))
 
+-- Función auxiliar para leer un árbol de Hoffman desde una cadena.
 readHoffman :: String -> [(Hoffman, String)]
 readHoffman input = case lex input of
   [("Hoja", resto)] -> [(Hoja char, resto'') | (char, resto'') <- reads resto]
@@ -41,24 +45,31 @@ readHoffman input = case lex input of
     , (")", resto'''') <- lex resto''']
   _ -> []
 
+-- Función para crear una nueva hoja de Hoffman a partir de un carácter.
 nuevoHoffman :: Char -> Hoffman
 nuevoHoffman a = Hoja a
 
+-- Función para fusionar dos árboles de Hoffman, creando un nuevo nodo.
 fusionHoffman :: Hoffman -> Hoffman -> Hoffman
 fusionHoffman izq der = Nodo izq der
 
+-- Función para obtener el carácter contenido en una hoja de Hoffman.
 obtenerCaracter :: Hoffman -> Char
 obtenerCaracter (Hoja a) = a
 
+-- Función para obtener el subárbol izquierdo de un nodo de Hoffman.
 arbolIzquierdo :: Hoffman -> Hoffman
 arbolIzquierdo (Nodo izq _) = izq
 
+-- Función para obtener el subárbol derecho de un nodo de Hoffman.
 arbolDerecho :: Hoffman -> Hoffman
 arbolDerecho (Nodo _ der) = der
 
+-- Función para generar la codificación de Hoffman de los caracteres en el árbol.
 codificacion :: Hoffman -> Map.Map Char String
 codificacion arbol = construirMapa arbol ""
 
+-- Función auxiliar para construir el mapa de codificaciones de Hoffman.
 construirMapa :: Hoffman -> String -> Map.Map Char String
 construirMapa (Hoja a) camino = Map.singleton a camino
 construirMapa (Nodo izq der) camino =
